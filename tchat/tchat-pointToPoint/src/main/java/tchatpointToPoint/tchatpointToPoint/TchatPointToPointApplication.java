de3ff71e19f2691ceef320dc33b9a81fb91c2d35
CHARTIER BAPTISTE <BCH17450@open-groupe.com>
2018-06-29 10:41:06 +0200

    first commit yeaaaah

---
+++ tchat/tchat-pointToPoint/src/main/java/tchatpointToPoint/tchatpointToPoint/TchatPointToPointApplication.java
@@ -0,0 +1,12 @@
+package tchatpointToPoint.tchatpointToPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TchatPointToPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TchatPointToPointApplication.class, args);
+	}
+}
